a6d4cc696f5baf26143411907975738903306749
Kayla Cross <kac288@georgetown.edu>
2018-07-23 20:09:36 +0000

    automatically backed up by learn

--- db/migrate/004_create_actors.rb
+++ db/migrate/004_create_actors.rb
@@ -1,8 +1,10 @@
 class CreateActors < ActiveRecords::Migration[4.2]
 
   def change
+    
     create_table :actors do |t|
-      t.string :name
+      t.string :first_name
+      t.string :last_name
     end
 
   end
